965289380313fbcf4e1259a23fba142fdc7d388e
Stratos Gerakakis <stratosgear@gmail.com>
2016-11-18 22:48:12 +0100

    Autosay Yes on package installations.

--- 02.docker-install.sh
+++ 02.docker-install.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
 
-# Updateable params:
-$lvmdisk=/dev/sdc
-
 # Instalation script from:
 # https://docs.docker.com/engine/installation/linux/ubuntulinux/
 
-
-
 # Update your apt sources
 sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates
+sudo apt-get -y install apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt-get update
 apt-cache policy docker-engine
 
 # Prerequisites by Ubuntu Version
-sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
 
 # Install Docker
-sudo apt-get install docker-engine
-sudo service docker start
+sudo apt-get -y install docker-engine
+#sudo service docker start
 
 # make docker start on boot
 sudo systemctl enable docker
@@ -56,5 +51,3 @@ sudo service docker start
 # Verify everything is working ok
 docker run hello-world
 docker info
-
-
